2a4ecaed7dea43d6185fe12b67d6a0c2e7f53309
Rx124 <akshat_kumar_mishra@rocketmail.com>
2021-06-19 17:32:27 +0530

    tailwind css added

--- vit-ranking/src/App.js
+++ vit-ranking/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="bg-red-700">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        </div>
       </header>
     </div>
   );

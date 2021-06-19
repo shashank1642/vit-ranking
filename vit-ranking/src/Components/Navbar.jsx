a4ecf131a3a523c55b46c60fb41660cdc623c031
Rx124 <akshat_kumar_mishra@rocketmail.com>
2021-06-20 01:12:31 +0530

    landing update

--- vit-ranking/src/Components/Navbar.jsx
+++ vit-ranking/src/Components/Navbar.jsx
@@ -7,14 +7,9 @@ function Navbar() {
   return (
     <div className="mx-8 text-2xl h-14">
       
-      {/* <img src={Logo} alt="logo" className="h-16 w-16"></img>
-      <p className="text-white pt-8">Rank Vitians</p> */}
-
-
-      {/* <a href="">
-        <img src={Logo} alt="logo" className="w-28 h-28"></img>
-      </a> */}
-      <div className="float-right">
+      <img src={Logo} alt="logo" className="h-16 w-16 inline-block"></img>
+      <p className="text-white inline-block"> Rank Vitians</p>
+      <div className="float-right ">
         <a href="" className="px-4 text-white">
           GPA Calculator
         </a>
--- vit-ranking/src/Pages/LandingPage.jsx
+++ vit-ranking/src/Pages/LandingPage.jsx
@@ -4,14 +4,14 @@ import Man from "../images/man-on-book.svg";
 
 function LandingPage() {
   return (
-    <div className="mx-8 mt-4  h-96">
-      <div className="float-left w-1/2">
+    <div className="mx-8 mt-4  h-96 grid grid-flow-col">
+      <div className="float-left">
         <p className="text-5xl text-white mt-60">
           A Fresh Take on Ranking System with evaluation of GPA , CGPA and
           Estimate your CGPA for further improvement
         </p>
       </div>
-      <div className="float-left w-1/2">
+      <div className="float-left">
       <img src={Man} alt="Man" ></img>
       </div>
     </div>

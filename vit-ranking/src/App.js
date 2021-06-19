6878ba4a26b4a12a25febe907601a3305d63e44a
siddarthamareedu <mareedusiddartha@gmail.com>
2021-06-19 19:05:31 +0530

    Navbar

--- vit-ranking/src/App.js
+++ vit-ranking/src/App.js
@@ -1,27 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import './index.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="bg-red-700">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        </div>
-      </header>
-    </div>
+    <Navbar/>
   );
 }
 
---
+++ vit-ranking/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Navbar() {
+    return(
+        <div className="Color">
+      <div className="mx-8 text-2xl">
+        <a href="" className="text-green-900">Logo</a>
+
+      </div>
+      
+    </div>
+    );
+}
+
+export default Navbar;
